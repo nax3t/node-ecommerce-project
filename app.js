e815a67b9c36ee82a56b7e08cf03db0fec917541
Ian Schoonover <iantskon@gmail.com>
2017-04-20 20:37:06 -0500

    Add sample product info

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const keyPublishable = process.env.PUBLISHABLE_KEY;
-const keySecret = process.env.SECRET_KEY;
-
-const app = require("express")();
-const stripe = require("stripe")(keySecret);
-const engine = require('ejs-mate');
-
 // dot env
 require('dotenv').config();
 
+const keyPublishable = process.env.PUBLISHABLE_KEY;
+const keySecret = process.env.SECRET_KEY;
+const express = require("express");
+const app = express();
+const stripe = require("stripe")(keySecret);
+const engine = require('ejs-mate');
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
 // use ejs-locals for all ejs templates: 
 app.engine('ejs', engine);
-
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>
-  res.render("index", {keyPublishable}));
+  res.render("index", {keyPublishable: keyPublishable}));
 
 app.post("/charge", (req, res) => {
   let amount = 500;
-
   stripe.customers.create({
     email: req.body.stripeEmail,
     source: req.body.stripeToken
@@ -35,4 +37,6 @@ app.post("/charge", (req, res) => {
   .then(charge => res.render("charge"));
 });
 
-app.listen(4567);
+app.listen(3000, () => {
+	console.log('Server running, listening on port 3000');
+});
